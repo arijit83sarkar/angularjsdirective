c06a39738c8e18efe11c206e05353a0bc4d86dea
Arijit Sarkar <mail2arijit83@gmail.com>
2017-04-28 15:16:20 +0530

    Update MainController.js

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,2 +1,17 @@
 app.controller('MainController', ['$scope', function($scope){
+  $scope.book1 = {
+    name: 'Glass Palace',
+    price: 2.56,
+    pubdate: new Date('2010', '04', '16')
+  };
+  $scope.book2 = {
+    name: 'Motor Cycle Diaries',
+    price: 2.86,
+    pubdate: new Date('2011', '02', '20')
+  };
+  $scope.book3 = {
+    name: 'Sea of Popies',
+    price: 2.20,
+    pubdate: new Date('2010', '12', '16')
+  };
 }]);
