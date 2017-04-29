3b76f005c27b5095901cf942fedb63ad9d73d3a8
Arijit Sarkar <mail2arijit83@gmail.com>
2017-04-29 12:57:56 +0530

    Update MainController.js

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,17 +1,20 @@
 app.controller('MainController', ['$scope', function($scope){
   $scope.book1 = {
+    icon: 'img/book1.png',
     name: 'Glass Palace',
     author: 'Amitav Ghosh',
     price: 2.56,
     pubdate: new Date('2010', '04', '16')
   };
   $scope.book2 = {
+    icon: 'img/book2.png',
     name: 'Motor Cycle Diaries',
     author: 'Che Guyevara',
     price: 2.86,
     pubdate: new Date('2011', '02', '20')
   };
   $scope.book3 = {
+    icon: 'book3.png',
     name: 'Sea of Popies',
     author: 'Amitav Ghosh',
     price: 2.20,
