919ca56a44488622e52c6024e3c996ecfdf546dd
Arijit Sarkar <mail2arijit83@gmail.com>
2017-05-02 22:22:55 +0530

    Update MainController.js

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -26,6 +26,28 @@ app.controller('MainController', ['$scope', function($scope){
   
   $scope.apps = [
     {
+      icon: 'img/book1.png',
+      name: 'Glass Palace',
+      author: 'Amitav Ghosh',
+      price: 2.56
+    },
+    {
+      icon: 'img/book2.png',
+      name: 'Motor Cycle Diaries',
+      author: 'Che Guyevara',
+      price: 2.32
+    },
+    {
+      icon: 'img/book3.png',
+      name: 'Sea of Popies',
+      author: 'Amitav Ghosh',
+      price: 2.20
+    },
+    {
+      icon: 'img/book4.png',
+      name: 'Seasons of Ghost',
+      author: 'Ruskin Bond',
+      price: 2.75
     }
     ];
 }]);
